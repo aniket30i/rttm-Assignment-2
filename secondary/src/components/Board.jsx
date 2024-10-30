98bc5172af7580fd89be5893eb6cd993b2595d38
Aniket Saha <iamaniket.saha.09@protonmail.com>
2024-10-31 02:38:38 +0530

    task viewer designs and table

--- secondary/src/components/Board.jsx
+++ secondary/src/components/Board.jsx
@@ -9,28 +9,35 @@ const Board = () => {
   if (error) return <div>Error: {error}</div>;
 
   return (
-    <div className="flex">
-      {tasks.length > 0 ? (
-        tasks.map((task) => (
-          <div key={task.id}>
-            <p>
-              <strong>Task ID:</strong> {task.id}
-            </p>
-            <p>
-              <strong>Task:</strong> {task.task}
-            </p>
-            <p>
-              <strong>Status:</strong> {task.status}
-            </p>
-            <p>
-              <strong>Assigned To:</strong> {task.assigned_to}
-            </p>
-            <hr />
-          </div>
-        ))
-      ) : (
-        <div>No tasks available</div>
-      )}
+    <div className="mt-20 w-2/3 ml-auto mr-auto border-2 border-yellow-400 p-1 rounded-lg">
+      <div>
+        <input
+          type="text"
+          id="Search"
+          placeholder="Search by Assign ID"
+          className="border-2 border-yellow-400 p-1 mb-2 rounded-lg"
+        />
+      </div>
+      <table className="table table-hover">
+        <thead>
+          <tr>
+            <td>Task ID</td>
+            <td>Task</td>
+            <td>Status</td>
+            <td>Assigned To</td>
+          </tr>
+        </thead>
+        <tbody>
+          {tasks.map((task) => (
+            <tr key={task.id}>
+              <td>{task.id}</td>
+              <td>{task.task}</td>
+              <td>{task.status}</td>
+              <td>{task.assigned_to}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 };
