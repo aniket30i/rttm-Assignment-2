ec2f0bafde8af1abe63959546071adf7cdbc96f8
Aniket Saha <iamaniket.saha.09@protonmail.com>
2024-10-31 02:20:32 +0530

    started with the task board design

--- secondary/src/components/Board.jsx
+++ secondary/src/components/Board.jsx
@@ -4,11 +4,33 @@ const Board = () => {
   const { tasks, isLoading, error } = useFetchTask(
     "http://localhost:3080/tasks"
   );
+
+  if (isLoading) return <div>Loading...</div>;
+  if (error) return <div>Error: {error}</div>;
+
   return (
-    <div>
-      {tasks.map((task, index) => (
-        <div key={index}>{task}</div>
-      ))}
+    <div className="flex">
+      {tasks.length > 0 ? (
+        tasks.map((task) => (
+          <div key={task.id}>
+            <p>
+              <strong>Task ID:</strong> {task.id}
+            </p>
+            <p>
+              <strong>Task:</strong> {task.task}
+            </p>
+            <p>
+              <strong>Status:</strong> {task.status}
+            </p>
+            <p>
+              <strong>Assigned To:</strong> {task.assigned_to}
+            </p>
+            <hr />
+          </div>
+        ))
+      ) : (
+        <div>No tasks available</div>
+      )}
     </div>
   );
 };
