3e7baa062d59be43d24a41a7cb2e0398c75f223e
Aniket Saha <iamaniket.saha.09@protonmail.com>
2024-11-02 03:35:53 +0530

    some more fixes regarding re render of taskboard

--- secondary/src/App.jsx
+++ secondary/src/App.jsx
@@ -1,8 +1,9 @@
 import Provider from "./context/provider";
 import Taskboard from "./components/Taskboard";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [updateTrigger, setUpdateTrigger] = useState(0);
   useEffect(() => {
     const ws = new WebSocket("ws://localhost:8080");
     ws.onopen = () => {
@@ -13,11 +14,12 @@ function App() {
       const notificationData = JSON.parse(event.data);
       alert(`Notification: ${notificationData.message}`);
       console.log(`Received notification: ${notificationData.message}`);
+      setUpdateTrigger((prev) => prev + 1);
     };
-  }, []);
+  }, [setUpdateTrigger]);
   return (
     <Provider>
-      <Taskboard />
+      <Taskboard trigger={updateTrigger} />
     </Provider>
   );
 }
